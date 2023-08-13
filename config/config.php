e488238cb7e0f999cebb2f51268c6025f3277b2b
hembi <hembi7@gmail.com>
2023-08-13 17:52:03 +0200

    Test DB

--- config/config.php
+++ config/config.php
@@ -10,8 +10,6 @@ final class Config
     const DB_PORT       = '3306';
     const DB_USERNAME   = 'root';
     const DB_PASSWORD   = '';
-    
-    const PASSWORD_SALT = "mUqfKzDkFFWZsM8ZBD6DUG9KoKyrTdT9";
 }
 
 ?>
